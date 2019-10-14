e7b89605f12dee013f9043d9b88fe6c37c89f13d
shin <kouokukou@yahoo.co.jp>
2019-10-14 16:49:37 +0900

    goAPI

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,10 @@ RUN go get github.com/jinzhu/gorm
 RUN go get github.com/jinzhu/gorm/dialects/postgres
 RUN go get github.com/new1990/gosmaple/db
 RUN go get github.com/new1990/gosmaple/entity
+RUN go get github.com/new1990/gosmaple/service
+RUN go get github.com/new1990/gosmaple/controller
+RUN go get github.com/new1990/gosmaple/server
+
 
 EXPOSE 8080
 
--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 import (
-      "github.com/gin-gonic/gin"
-      "github.com/new1990/gosmaple/db" 
+      "github.com/new1990/gosmaple/server"
+      "github.com/new1990/gosmaple/db"
 
 
      )
 
 func main() {
-    r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
+    // r := gin.Default()
+    // r.GET("/ping", func(c *gin.Context) {
+    //     c.JSON(200, gin.H{
+    //         "message": "pong",
+    //     })
+    // })
     db.Init()
-    r.Run()
+    // r.Run()
+    server.Init()
     db.Close()
 }
